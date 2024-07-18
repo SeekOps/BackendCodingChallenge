3400d76ed2751f14e538086dbe3d8a96218bda91
Trent Frederick <tfrederick@seekops.com>
2024-07-18 12:26:37 -0500

    Updated the test dataset and added erd diagram

--- main_project/api/views.py
+++ main_project/api/views.py
@@ -6,7 +6,5 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from .serializers import *
-from .utils.file_upload import csv_upload
+from .utils.file_upload import *
 from .models import Measurement, Inspection
-
-
